c082729dd38064d6476b11a932a4f3ffeec934e9
James Huaman <james.huaman@tecsup.edu.pe>
2026-09-01 17:24:50 -0500

    Añadi StudentController

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,5 +1,15 @@
 package pe.edu.tecsup.lab03.controllers;
 
-public class StudentController {
+import java.util.ArrayList;
 
-}
+public class StudentController {
+    public ArrayList<String> ListaEstudiantes = new ArrayList<>();
+
+    public void Listar(){
+        String[] listaEstudiante = new String[0];
+        for(String estudiante : listaEstudiante) {
+            System.out.println(estudiante);
+        }
+        }
+    }
+
